c221cfa7995ebd4c46c87fdbeb4f2acb8989634b
lavylipesh <lilibethlangat@yahoo.com>
2019-10-06 22:25:15 +0300

    change time zone

--- peshgallery/settings.py
+++ peshgallery/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'gallery',
         'USER':'lilibeth',
         'PASSWORD':'1234',
-        
+
     }
 }
 
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
