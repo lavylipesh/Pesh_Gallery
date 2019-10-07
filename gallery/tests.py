76a5615cf5510bedffe81df93dc9f71801607bf1
lavylipesh <lilibethlangat@yahoo.com>
2019-10-07 10:00:23 +0300

    add tests for loacation class

--- gallery/tests.py
+++ gallery/tests.py
@@ -4,10 +4,9 @@ from .models import Category,Location,Image
 class CategoryTestClass(TestCase):
     # Set up method
     def setUp(self):
-        self.animation= Category(name = 'Animatons')
+        self.animation= Category(name = 'Animations')
    
-   
-    def test_instance(self):
+   def test_instance(self):
         self.assertTrue(isinstance(self.animation,Category))
     
     
@@ -15,6 +14,26 @@ class CategoryTestClass(TestCase):
         self.animation.save_category()
         categories = Category.objects.all()
         self.assertTrue(len(categories) > 0)
+
+class LocationTestClass(models.Model):
+    def setUp(self):
+         
+        self.category= Category(name = 'Animations')
+        self.animation.save_animation()
+
+     
+        self.new_image = image(name = 'image')
+        self.new_image.save()
+
+        self.new_location= Location(title = 'Test Location',post = 'This is a random test Post',category = self.animation)
+        self.new_location.save()
+
+        self.new_location.category.add(self.new_category)
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        image.objects.all().delete()
+        Location.objects.all().delete()
     
 
 
