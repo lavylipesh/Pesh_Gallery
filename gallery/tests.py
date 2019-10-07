afba0ae22da6b3fdf160a9a7abaf44f6bbf54814
lavylipesh <lilibethlangat@yahoo.com>
2019-10-07 09:20:47 +0300

    create a test for save method

--- gallery/tests.py
+++ gallery/tests.py
@@ -7,6 +7,10 @@ class CategoryTestClass(TestCase):
         self.animation= Category(name = 'Animatons')
     def test_instance(self):
         self.assertTrue(isinstance(self.animation,Category))
+    def test_save_method(self):
+        self.animations.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(editors) > 0)       
 
 
 
