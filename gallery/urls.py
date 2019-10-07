20691a58ad7c9f3a2db845958c3bd6ff156eac95
lavylipesh <lilibethlangat@yahoo.com>
2019-10-07 10:40:22 +0300

    displaying images

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.index,name ='index'),
+    url(r'^$',views.index,name ='index')
     url(r'^search/', views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
