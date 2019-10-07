7324751f72e348f6b907283a93513f1ae051ec10
lavylipesh <lilibethlangat@yahoo.com>
2019-10-07 09:08:50 +0300

    create Image model

--- gallery/models.py
+++ gallery/models.py
@@ -11,5 +11,13 @@ class Location(models.Model):
     name = models.CharField(max_length = 30)
 
     def __str__(self):
-        return self.name       
+        return self.name 
+
+class Image(models.Model):
+    image = models.ImageField(upload_to 'static/')
+    name = models.CharField(max_length = 60)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
     
